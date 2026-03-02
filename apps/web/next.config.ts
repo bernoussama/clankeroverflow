506a2d0b71ff2998afc6e3c29eb16fe307b5181f
bernoussama <oussama.bernou@outlook.com>
2026-03-02 22:27:36 +0000

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,13 @@
+import "@clankeroverflow/env/web";
+import type { NextConfig } from "next";
+
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
+
+initOpenNextCloudflareForDev();
